8f548541838742c09ceee8be5c14bc7ecea72873
leonardz17 <71532759+leonardz17@users.noreply.github.com>
2021-01-12 21:57:46 -0800

    updated scrape_mars and index.html files

--- app.py
+++ app.py
@@ -2,17 +2,20 @@
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
 import scrape_mars
+import pymongo
 
 # create instance of Flask app
 app = Flask(__name__)
 
-mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_info")
+conn = "mongodb://localhost:27017"
+client = pymongo.MongoClient(conn)
 
+db = client.mars_db
 # create route that renders index.html template
 @app.route("/")
 def home():
 
-    mars_data = mongo.db.mars_site.find_one()
+    mars_data = db.mars_info.find_one()
     print(mars_data)
     return render_template("index.html",  mars = mars_data)
 
@@ -23,7 +26,7 @@ def scrape():
     mars_all = scrape_mars.scrape_info()
 
     # Update the Mongo database using update and upsert=True
-    mongo.db.mars_site.update({}, mars_all, upsert=True)
+    # db.mars_info.update({}, mars_all, upsert=True)
 
     # Redirect back to home page
     return redirect("/")
--- scrape_mars.py
+++ scrape_mars.py
@@ -1,7 +1,16 @@
 from splinter import Browser
 from bs4 import BeautifulSoup
+from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+
 import time
+import requests
+import pymongo
+
+conn = "mongodb://localhost:27017"
+client = pymongo.MongoClient(conn)
+
+db = client.mars_db
 
 def init_browser():
     executable_path = {'executable_path': ChromeDriverManager().install()}
@@ -9,7 +18,7 @@ def init_browser():
 
 def scrape_info():
     browser = init_browser() 
-    mars_dict = {}   
+    mars_all = {}   
 
     # First website to scrape for news title and paragraph
     url = "https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest"
@@ -18,35 +27,64 @@ def scrape_info():
     html = browser.html
     soup = BeautifulSoup(html, "html.parser")
 
-    mars_dict["news_title"] = soup.find('div',class_='content_title').text
-    mars_dict["news_p"] = soup.find('div',class_='rollover_description_inner').text
+    results = soup.find_all('ul',class_='item_list')
+    for result in results:
 
-    browser.quit()
+        news_title = result.find('div',class_='content_title').text
+        mars_all["news_title"] = news_title
+
+        news_p = result.find('div',class_='article_teaser_body').text
+        mars_all["news_p"] = news_p
 
     # Second website to scrape for image
-    url = "https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars"
+    url = "https://www.jpl.nasa.gov"
+
     browser.visit(url)
 
+    xpath = '/html/body/div/div/div/header/div[1]/div[3]/div/nav/div[1]/div[4]/button/span'
+    browser.find_by_xpath(xpath).click()
+
     time.sleep(1)
-    browser.click_link_by_partial_text("FULL IMAGE")
-    browser.click_link_by_partial_text("more info")
+    xpath = '/html/body/div/div/div/header/div[1]/div[3]/div/nav/div[1]/div[4]/div/div/div/div/div[1]/div/div/div/a/p[1]'
+    browser.find_by_xpath(xpath).click()
+
+    xpath = '/html/body/div/div/div/main/div/div[2]/div/div/div[2]/button/span'
+    browser.find_by_xpath(xpath).click()
+
 
     html = browser.html
-    soup = BeautifulSoup(html, "html.parser")
+    soup = BeautifulSoup(html, 'html.parser')
+    # results = soup.body.main.find_all('img')
+    results = soup.find_all('div', class_='BaseLightbox__slide__img')
 
-    results = soup.find_all('figure', class_="lede")
     for result in results:
-        image_url = result.a['href']
-        mars_dict["featured_image_url"] = f"https://www.jpl.nasa.gov{image_url}"
-
-    browser.quit()
-    return mars_dict
+        mars_all["featured_image_url"] = result.img['src']
+    
     # Third website to scrape for image url and title of the 4 hemispheres
-    # url = "https://astrogeology.usgs.gov/search/map/Mars/Viking/cerberus_enhanced"
-    # browser.visit(url)
+    url = "https://astrogeology.usgs.gov/search/map/Mars/Viking/cerberus_enhanced"
+    
 
     ## List containing the names of the 4 hemispheres
-    # mars_hemispheres = ['cerberus', 'schiaparelli', 'syrtis_major', 'valles_marineris']
+    mars_hemispheres = ['cerberus', 'schiaparelli', 'syrtis_major', 'valles_marineris']
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
 
+    for hemisphere in mars_hemispheres:
+        mars_url = f'https://astrogeology.usgs.gov/search/map/Mars/Viking/{hemisphere}_enhanced'
+        response = requests.get(mars_url)
+        soup = BeautifulSoup(response.text, "html.parser")
+        results = soup.find_all("img", class_='wide-image')
+    
+        for result in results:
+            img_url = result.get('src')
+            title = soup.find("h2", class_='title').text
+            title = title.replace(' Enhanced',"")
+            mars_all[f"{hemisphere}_title"] = title
+            mars_all[f"{hemisphere}_img_url"] = "https://astrogeology.usgs.gov" + img_url
+    
+    db.mars_info.insert_one(mars_all)
 
+    browser.quit()
+    
+    return mars_all
 
